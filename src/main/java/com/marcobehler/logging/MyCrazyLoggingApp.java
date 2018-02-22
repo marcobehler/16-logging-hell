16fdbb6727ec7d662fb67477ebfee05871a11f2c
Marco Behler <marco@marcobehler.com>
2018-02-22 10:10:00 +0100

    slf4j basics

--- src/main/java/com/marcobehler/logging/MyCrazyLoggingApp.java
+++ src/main/java/com/marcobehler/logging/MyCrazyLoggingApp.java
@@ -1,13 +1,27 @@
 package com.marcobehler.logging;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
 /**
  * Thanks for watching this episode! Send any feedback to info@marcobehler.com!
  */
 public class MyCrazyLoggingApp {
 
     public static void main(String[] args) {
+
+        MDC.put("userId","this is my user id");
+
         // SLF4J
+        Logger logger = LoggerFactory.getLogger(MyCrazyLoggingApp.class);
+        logger.info("This is my log!!!!");
+
+
+        int numOfProblems = 5;
+        logger.info("user ID ..... Houston, we have {} problems!",  numOfProblems);
+
 
     }
 }
