434877d5f223b1c749d2b0c3800a10205ec276ee
Marco Behler <marco@marcobehler.com>
2018-05-18 11:11:20 +0200

    log4j2 logging

--- src/main/java/com/marcobehler/logging/MyCrazyLoggingApp.java
+++ src/main/java/com/marcobehler/logging/MyCrazyLoggingApp.java
@@ -13,7 +13,7 @@ public class MyCrazyLoggingApp {
     public static void main(String[] args) throws InterruptedException {
 
         while (true) {
-            Thread.sleep(250);
+            Thread.sleep(10);
             // Slf4j + Log4jV2
             org.slf4j.Logger logger = LoggerFactory.getLogger(MyCrazyLoggingApp.class);
             logger.info("Hey, this is Log4J - V2!");
